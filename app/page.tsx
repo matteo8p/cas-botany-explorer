2a1b2464288ac4fd9d31746c149f5001be565818
Matthew Wang <matt8p@outlook.com>
2025-02-27 22:54:56 -0800

    push

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,17 @@ export default function Home() {
   return (
     <>
       <main className="p-8 flex flex-col gap-8">
-        <h1 className="text-4xl font-bold text-center">Image Upload Demo</h1>
+        <h1 className="text-4xl font-bold text-center">CAS Zooniverse Demo</h1>
+        <p className="text-center text-gray-500">
+          Matthew Wang & Marcelo Jimenez
+        </p>
+        <div className="flex justify-center">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/en/2/26/California_Academy_of_Sciences_Logo.png"
+            alt="CAS Logo"
+            className="w-48 h-auto"
+          />
+        </div>
         <ImageUpload />
       </main>
     </>
