1c3ae0cb71e6033c5b6964fd7214246ff3686ff7
Matthew Wang <matt8p@outlook.com>
2025-02-27 23:32:52 -0800

    convex only

--- components/ConvexClientProvider.tsx
+++ components/ConvexClientProvider.tsx
@@ -2,8 +2,8 @@
 
 import { ReactNode } from "react";
 import { ConvexReactClient } from "convex/react";
-import { ConvexProviderWithClerk } from "convex/react-clerk";
-import { useAuth } from "@clerk/nextjs";
+import { ConvexProvider } from "convex/react";
+
 const convex = new ConvexReactClient(process.env.NEXT_PUBLIC_CONVEX_URL!);
 
 export default function ConvexClientProvider({
@@ -11,9 +11,5 @@ export default function ConvexClientProvider({
 }: {
   children: ReactNode;
 }) {
-  return (
-    <ConvexProviderWithClerk client={convex} useAuth={useAuth}>
-      {children}
-    </ConvexProviderWithClerk>
-  );
+  return <ConvexProvider client={convex}>{children}</ConvexProvider>;
 }
